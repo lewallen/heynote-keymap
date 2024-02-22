f061d320d7393cbe123cb5405464e5796129b6be
Sam Lewallen <sam.lewallen@gmail.com>
2024-02-22 04:41:32 -0500

    Update keymap.js
    ~

--- src/editor/keymap.js
+++ src/editor/keymap.js
@@ -1,44 +1,59 @@
-import { keymap } from "@codemirror/view"
+import { keymap } from "@codemirror/view";
 //import { EditorSelection, EditorState } from "@codemirror/state"
+import { indentLess, indentMore } from "@codemirror/commands";
+/* -------------------------------- FROM SAM -------------------------------- */
 import {
-    indentLess, indentMore, 
-} from "@codemirror/commands"
+    foldAll,
+    unfoldAll,
+    unfoldCode,
+    foldCode,
+    toggleFold
+} from "@codemirror/language";
+/* -------------------------------------------------------------------------- */
+import {
+    insertNewBlockAtCursor,
+    addNewBlockBeforeCurrent,
+    addNewBlockAfterCurrent,
+    addNewBlockBeforeFirst,
+    addNewBlockAfterLast,
+    moveLineUp,
+    moveLineDown,
+    selectAll,
+    gotoPreviousBlock,
+    gotoNextBlock,
+    selectNextBlock,
+    selectPreviousBlock,
+    gotoPreviousParagraph,
+    gotoNextParagraph,
+    selectNextParagraph,
+    selectPreviousParagraph,
+    newCursorBelow,
+    newCursorAbove
+} from "./block/commands.js";
+import { pasteCommand, copyCommand, cutCommand } from "./copy-paste.js";
 
-import { 
-    insertNewBlockAtCursor, 
-    addNewBlockBeforeCurrent, addNewBlockAfterCurrent,
-    addNewBlockBeforeFirst, addNewBlockAfterLast,
-    moveLineUp, moveLineDown, 
-    selectAll, 
-    gotoPreviousBlock, gotoNextBlock, 
-    selectNextBlock, selectPreviousBlock,
-    gotoPreviousParagraph, gotoNextParagraph, 
-    selectNextParagraph, selectPreviousParagraph,
-    newCursorBelow, newCursorAbove,
-} from "./block/commands.js"
-import { pasteCommand, copyCommand, cutCommand } from "./copy-paste.js"
-
-import { formatBlockContent } from "./block/format-code.js"
-import { deleteLine } from "./block/delete-line.js"
-
+import { formatBlockContent } from "./block/format-code.js";
+import { deleteLine } from "./block/delete-line.js";
 
 export function keymapFromSpec(specs) {
-    return keymap.of(specs.map((spec) => {
-        if (spec.run) {
-            if ("preventDefault" in spec) {
-                return spec
+    return keymap.of(
+        specs.map(spec => {
+            if (spec.run) {
+                if ("preventDefault" in spec) {
+                    return spec;
+                } else {
+                    return { ...spec, preventDefault: true };
+                }
             } else {
-                return {...spec, preventDefault: true}
+                const [key, run] = spec;
+                return {
+                    key,
+                    run,
+                    preventDefault: true
+                };
             }
-        } else {
-            const [key, run] = spec
-            return {
-                key,
-                run,
-                preventDefault: true,
-            }
-        }
-    }))
+        })
+    );
 }
 
 export function heynoteKeymap(editor) {
@@ -54,6 +69,10 @@ export function heynoteKeymap(editor) {
         ["Mod-Enter", addNewBlockAfterCurrent],
         ["Mod-Alt-Enter", insertNewBlockAtCursor],
         ["Mod-a", selectAll],
+        ["Mod-[", foldCode],
+        ["Mod-]", unfoldCode],
+        ["Mod-Shift-,", foldAll],
+        ["Mod-Shift-.", unfoldAll],
         ["Alt-ArrowUp", moveLineUp],
         ["Alt-ArrowDown", moveLineDown],
         ["Mod-l", () => editor.openLanguageSelector()],
@@ -61,9 +80,43 @@ export function heynoteKeymap(editor) {
         ["Mod-Alt-ArrowDown", newCursorBelow],
         ["Mod-Alt-ArrowUp", newCursorAbove],
         ["Mod-Shift-k", deleteLine],
-        {key:"Mod-ArrowUp", run:gotoPreviousBlock, shift:selectPreviousBlock},
-        {key:"Mod-ArrowDown", run:gotoNextBlock, shift:selectNextBlock},
-        {key:"Ctrl-ArrowUp", run:gotoPreviousParagraph, shift:selectPreviousParagraph},
-        {key:"Ctrl-ArrowDown", run:gotoNextParagraph, shift:selectNextParagraph},
-    ])
+        {
+            key: "Mod-ArrowUp",
+            run: gotoPreviousBlock,
+            shift: selectPreviousBlock
+        },
+        { key: "Mod-ArrowDown", run: gotoNextBlock, shift: selectNextBlock },
+        {
+            key: "Ctrl-ArrowUp",
+            run: gotoPreviousParagraph,
+            shift: selectPreviousParagraph
+        },
+        {
+            key: "Ctrl-ArrowDown",
+            run: gotoNextParagraph,
+            shift: selectNextParagraph
+        }
+        /* ---------------------------- FROM SAM ---------------------------- */
+        // {
+        //     key: "Mod-[",
+        //     run: foldCode
+        // },
+        // {
+        //     key: "Mod-]",
+        //     run: unfoldCode
+        // },
+        // {
+        //     key: "Mod-Shift-,",
+        //     run: foldAll
+        // },
+        // {
+        //     key: "Mod-Shift-.",
+        //     run: unfoldAll
+        // },
+        // {
+        //     key: "Mod-k",
+        //     run: insertLink
+        // }
+        /* ------------------------------------------------------------------ */
+    ]);
 }
